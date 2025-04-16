0eee103f91d33668770029b306a2ed87dbc1242f
Sharjeel Khan <sharjeelkhan@google.com>
2025-04-16 20:31:15 +0000

    Add __builtin_dynamic_object_size and pass_dynamic_object_size
    With _FORTIFY_SOURCE=3, we can start using __builtin_dynamic_object_size
    and pass_dynamic_object_size to get better runtime checks for memory
    allocations. We just switch __bosn over to __builtin_dynamic_object_size
    and __pass_object_size_n over to __pass_dynamic_object_size__.
    
    Bug: 291762537
    Test: presubmits
    Change-Id: Ic2a5439f8e7f6ebd223bd8dfc4514400ec004de5

--- libc/include/sys/cdefs.h
+++ libc/include/sys/cdefs.h
@@ -247,12 +247,21 @@
 #  define __bos_level 0
 #endif
 
-#define __bosn(s, n) __builtin_object_size((s), (n))
+#if _FORTIFY_SOURCE >= 3
+#  define __bosn(s, n) __builtin_dynamic_object_size((s), (n))
+#else
+#  define __bosn(s, n) __builtin_object_size((s), (n))
+#endif
 #define __bos(s) __bosn((s), __bos_level)
 
 #if defined(__BIONIC_FORTIFY)
 #  define __bos0(s) __bosn((s), 0)
-#  define __pass_object_size_n(n) __attribute__((__pass_object_size__(n)))
+#  if _FORTIFY_SOURCE >= 3
+#    define __pass_object_size_n(n) __attribute__((__pass_dynamic_object_size__(n)))
+#  else
+#    define __pass_object_size_n(n) __attribute__((__pass_object_size__(n)))
+#  endif
+
 /*
  * FORTIFY'ed functions all have either enable_if or pass_object_size, which
  * makes taking their address impossible. Saying (&read)(foo, bar, baz); will
